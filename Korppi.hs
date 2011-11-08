288e2ec9f0fcaa06525a6ea171a55cec3e02cf5e
Heikki Salo <heikki.ao.salo@iki.fi>
2011-11-08 19:34:08 +0200

    Update login to support Korppi OpenID.

--- Korppi.hs
+++ Korppi.hs
@@ -1,6 +1,8 @@
 {-#LANGUAGE ScopedTypeVariables, ViewPatterns, RecordWildCards, OverloadedStrings#-}
 -- | This module provides means to log into the Korppi system and fetch classroom reservations
 module Korppi (login, reservations, Event(..)) where
+
+import System.IO.Unsafe
 import Network.Curl
 import Network.Curl.Easy
 import Network.Curl.Opts
@@ -15,6 +17,7 @@ import Data.Time.Clock
 import System.Locale
 import Control.Monad
 import Control.Monad.Trans
+import Data.Char
 
 import Utils
 import VMonad
@@ -43,15 +46,37 @@ reservations sessionCookie time = do
     let rooms = T.decodeUtf8 . respBody $ t
     readColumns rooms >>= mapM Korppi.parseEvent
 
+stripP = dropWhile isSpace
+
 -- |Exhange account and password for a session cookie.
 login :: String -> String -> VaksiMonad String
-login account password = do
-    resp  :: CurlResponse <- curling $ curlGetResponse_ loginURL
-                                        [CurlPostFields ["account=" ++account
-                                                        ,"password="++password]]
-    note (show $ respHeaders resp)
-    note (show $ respBody resp)
-    m2e "Login failed" $ lookup "Set-Cookie" $ respHeaders resp 
+-- FIXME: For some odd reason "curling $ curlGetResponse .."
+--        segfaults -> unsafePerformIO
+login account password = return $ unsafePerformIO $ do
+    -- | Begin login from frontpage, put SessionID aside
+    r1 <- curlGetResponse "https://korppi.jyu.fi/kotka/servlet/authentication/openIdLogin" []
+    let head1 = respHeaders r1
+        Just korppicookie = lookup "Set-Cookie" head1
+        -- | For some reason Location fields contain extra prefix space that breaks curl.
+        Just manageURL = fmap stripP $ lookup "Location" head1
+
+    -- | Continue to OpenID manager.
+    r2 <- curlGetResponse (tail manageURL) []
+    let postLoginFields = CurlPostFields ["username="++account, "password="++password, "login=Login"]
+
+    r3 <- curlGetResponse manageURL [postLoginFields]
+    let head3 = respHeaders r3
+        Just cookie1 = lookup "Set-Cookie" head3
+        Just return2KorppiURL = fmap stripP $ lookup "Location" head3
+
+    -- | Let's visit the returnto address once to say hi.
+    _ <- curlGetResponse return2KorppiURL [CurlHttpHeaders ["Cookie:"++korppicookie]]
+
+    -- | For non-JS agents this call must be made once:
+    let disableJSURL = "https://korppi.jyu.fi/kotka/noticeboard/notices.jsp?fromLogin=true&javascript-enabled=false"
+    _ <- curlGetResponse disableJSURL [CurlHttpHeaders ["Cookie:"++korppicookie]]
+
+    return korppicookie
 
 
 -- Parsing
